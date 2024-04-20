437420594f67a7b475ea9451fdfe0e142214c528
Ryan Montoyo <ryanmontoyo2003@gmail.com>
2024-04-20 19:27:32 -0400

    application + db

--- database.py
+++ database.py
@@ -31,3 +31,8 @@ def load_job_from_db(id):
         else:
             column_names = result.keys()
             return dict(zip(column_names, row))
+        
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text('INSERT INTO applications (job_id, first_name, last_name, email, linkedin_url, education, work_experience, resume_url) VALUES(:job_id, :first_name, :last_name, :email, :linkedin_url, :education, :work_experience, :resume_url)')
+        conn.execute(query, job_id = job_id, first_name = data['first_name'], last_name = data['last_name'], email = data['email'], linkedin_url = data['linkedin_url'],education = data['education'],work_experience = data['work_experience'], resume_url = data['resume_url'])
