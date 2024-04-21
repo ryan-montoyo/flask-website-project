d459af16e8ee04ab27d052b0e732022129b0526a
Ryan Montoyo <ryanmontoyo2003@gmail.com>
2024-04-20 20:16:22 -0400

    fixed database bug

--- app.py
+++ app.py
@@ -23,13 +23,14 @@ def show_job(id):
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
 
-@app.route('/job/<id>/apply', methods = ['post'])
+@app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
   data = request.form
   job = load_job_from_db(id)
-  
   add_application_to_db(id, data)
-  return render_template('application_submitted.html', application = data, job = job)
+  return render_template('application_submitted.html', 
+                         application=data,
+                         job=job)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = 'mysql+pymysql://sql5700633:5BtfaBzQmB@sql5.freemysqlhosting.net/sql5700633?charset=utf8mb4'
+db_connection_string = os.getenv('DB_CONNECTION_STRING')
 
 engine = create_engine(db_connection_string)
 
@@ -34,5 +34,19 @@ def load_job_from_db(id):
         
 def add_application_to_db(job_id, data):
     with engine.connect() as conn:
-        query = text('INSERT INTO applications (job_id, first_name, last_name, email, linkedin_url, education, work_experience, resume_url) VALUES(:job_id, :first_name, :last_name, :email, :linkedin_url, :education, :work_experience, :resume_url)')
-        conn.execute(query, job_id = job_id, first_name = data['first_name'], last_name = data['last_name'], email = data['email'], linkedin_url = data['linkedin_url'],education = data['education'],work_experience = data['work_experience'], resume_url = data['resume_url'])
+        query = text("INSERT INTO applications (job_id, first_name, last_name, email, linkedin_url, education, work_experience, resume_url)"
+                     "VALUES (:job_id, :first_name, :last_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        
+        params = {
+            'job_id': job_id,
+            'first_name': data['first_name'],
+            'last_name': data['last_name'],
+            'email': data['email'],
+            'linkedin_url': data['linkedin_url'],
+            'education': data['education'],
+            'work_experience': data['work_experience'],
+            'resume_url': data['resume_url']
+        }
+
+        conn.execute(query, params)
+        conn.commit()
